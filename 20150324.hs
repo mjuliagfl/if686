43c8f67a47d862d4609f14fa18813dfd61faf9d5
mjuliagfl <mjgfl@cin.ufpe.br>
2015-03-24 22:37:18 -0300

    Atividades da aula 24/03

--- 20150324.hs
+++ 20150324.hs
@@ -1,3 +1,5 @@
+--Códigos da atividade:
+
 --mergesort
 mergeSort :: [Int] -> [Int]
 mergeSort x
@@ -13,7 +15,7 @@ merge a b
  |a ==[] = b
  |b == [] = a
  |head a <= head b = head a:merge (tail a) b
- |otherwise = head b:merge a (tail b)]
+ |otherwise = head b:merge a (tail b)
 
 
  --Função auxiliar swap
@@ -111,3 +113,128 @@ heapsort (f:s)
 		heapsort (removeLast ((heapfy (swap (head (heapfy (f:s) (0) (length (f:s))))
 			(getVal (heapfy (f:s) (0) (length (f:s))) ((length (f:s))-1) (0))
 			(0) ((length (f:s))-1) (0) (heapfy (f:s) (0) (length (f:s)))) (0) ((length (f:s))-1))))
+
+
+
+--------------------------------------------------------------------------------------------------------------
+
+
+
+--Códigos da aula:
+
+
+--menorMaior
+menorMaior :: Int -> Int -> Int -> (Int, Int)
+menorMaior x y z = (menor x (menor y z), maior x (maior y z))
+
+maior :: Int -> Int -> Int
+maior x y 
+ |x> y = x
+ |otherwise = y
+
+menor :: Int -> Int -> Int
+menor x y 
+ |x < y = x
+ |otherwise = y
+
+--ordenaTripla
+ordenaTripla :: (Int, Int, Int) -> (Int, Int, Int)
+ordenaTripla (x, y, z)
+ |(fst (menorMaior x y z)) == x && (snd (menorMaior x y z)) == y = (x, z, y )
+ |(fst (menorMaior x y z) )== y && (snd (menorMaior x y z))== x = (y, z, x )
+ |(fst (menorMaior x y z)) == z && (snd(menorMaior x y z)) == y = (z, x, y )
+ |(fst (menorMaior x y z) )== z && (snd (menorMaior x y z)) == x= (z, y, x )
+ |(fst (menorMaior x y z) )== x && (snd (menorMaior x y z)) == z = (x, y, z)
+ |otherwise = (y,x,z)
+
+--ponto, reta, vertical
+type Ponto = (Float, Float)
+type Reta = (Ponto, Ponto)
+
+primeira :: Ponto -> Float
+primeira (a, b) = a
+
+segunda :: Ponto -> Float
+segunda (a, b) = b
+
+vertical :: Reta -> Bool
+vertical ((a, b), (c,d)) = (a == c)
+
+pontoY :: Float -> Reta -> Float
+pontoY px ((x1, y1), (x2, y2))
+ |x1==x2 = 0 -- se a reta for vertical y = infinito, portanto pode assummir qualquer valor
+ |otherwise = ((y2-y1)*(px-x1))/(x2-x1) + y1
+
+--biblioteca
+type Pessoa = String
+
+type Livro = String
+
+type BancoDados = [(Pessoa,Livro)]
+baseExemplo :: BancoDados
+
+baseExemplo = 
+ [("Sergio","O Senhor dos Aneis"),
+
+ ("Andre","Duna"),
+ 
+ ("Lucas","Duna"),
+ 
+ ("Fernando","Jonathan Strange & Mr. Norrell"), 
+
+ ("Fernando","A Game of Thrones")]
+
+-- livros emprestados
+
+livros :: BancoDados -> Pessoa -> [Livro]
+livros b p = [snd x | x <- b, (fst x == p)]
+
+emprestimos :: BancoDados -> Livro ->[Pessoa]
+emprestimos b l = [fst x | x <- b, (snd x == l)]
+ 
+emprestado :: BancoDados -> Livro -> Bool
+emprestado b l
+ |b == [] = False
+ |snd(head b) == l = True
+ |otherwise = emprestado (tail b) l
+ 
+qtdEmprestimos ::BancoDados -> Pessoa -> Int
+qtdEmprestimos b p = length (livros b p)
+
+emprestar :: BancoDados -> Pessoa -> Livro -> BancoDados
+emprestar b p l = b ++ [(p, l)]
+
+devolver :: BancoDados -> Pessoa -> Livro -> BancoDados
+devolver b p l 
+ |b == [] = []
+ |head b == (p,l) = devolver (tail b) p l 
+ |otherwise = head b:devolver (tail b) p l 
+
+--utilizando compreensão de listas:
+membro :: [Int] -> Int -> Bool
+membro x n = ([ l | l <- x, l == n]) /= []
+
+--livros :: BancoDados -> Pessoa -> [Livro]
+-- ja tihna feito dessa forma (coódigo mais acima)
+
+--emprestimos :: BancoDados -> Livro ->[Pessoa]
+-- ja tinha feito dessa forma (código mais acima)
+
+emprestado2 :: BancoDados -> Livro -> Bool
+emprestado2 b l = ([x | x <- b, snd x == l]) /= []
+
+qtdEmprestimos2 :: BancoDados -> Pessoa -> Int
+qtdEmprestimos2 b p = length ([x | x <- b, fst x == p])
+
+devolver2 :: BancoDados -> Pessoa -> Livro -> BancoDados
+devolver2 b p l = [x | x <- b, x /= (p, l)]
+
+--quicksort 
+
+quick[] = []
+quick(h:t) = quick[x | x <- t, x <= h] ++ h:quick[y|y<-t, y>h]
+
+
+
+
+
